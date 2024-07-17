652d4bc899027cfd1851582e298a2058a1e9dcfb
cabbott008 <curtisabbott@me.com>
2024-07-17 03:23:59 -0500

    fonts and tweaks

--- modules/home-manager/DPI-Low.nix
+++ modules/home-manager/DPI-Low.nix
@@ -13,27 +13,4 @@
     size = 32;
     package = pkgs.gnome.adwaita-icon-theme;
   };
-
-  # home.pointerCursor = 
-  #  let 
-  #    getFrom = url: hash: name: {
-  #        gtk.enable = true;
-  #        x11.enable = true;
-  #        name = name;
-  #        size = 48;
-  #        package = 
-  #          pkgs.runCommand "moveUp" {} ''
-  #            mkdir -p $out/share/icons
-  #            ln -s ${pkgs.fetchzip {
-  #              url = url;
-  #              hash = hash;
-  #            }} $out/share/icons/${name}
-  #        '';
-  #      };
-  #  in
-  #    getFrom 
-  #      "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Red.tar.gz"
-  #      "sha256-i91RzcANAfuaYEywaIzAnrpUl+8VhvLxQrn3NhCVNRA="
-  #     # "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
-  #      "Fuchsia-Red"; 
 }
--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -5,11 +5,13 @@
     enable = true;
     theme = "Adwaita dark";
     font = {
-      name = "JetBrainsMono Nerd Font Mono";
+      name = "JetBrainsMono Nerd Font";
       size = 12;
     };
     settings = {
       confirm_os_window_close = 0;
+      copy_on_select = true;
+      clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
     };
   };
 }
