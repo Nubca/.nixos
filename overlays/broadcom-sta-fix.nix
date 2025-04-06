74b8e589969a37002f2412dc82efa961d7ee8f25
Curtis Abbott <inspiredplans@gmail.com>
2025-04-05 19:57:46 -0500

    cleanup

--- overlays/broadcom-sta-fix.nix
+++
@@ -1,9 +0,0 @@
-self: super: {
-  broadcom-sta = super.broadcom-sta.overrideAttrs (oldAttrs: {
-    postPatch = (oldAttrs.postPatch or "") + ''
-      substituteInPlace src/wl/sys/wl_linux.c \
-        --replace '#include <asm/unaligned.h>' '#include <linux/unaligned.h>'
-    '';
-  });
-}
-
