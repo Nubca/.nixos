f4dd0a2acd72b6587fc299afc9ff71ed07969811
Curtis Abbott <inspiredplans@gmail.com>
2025-05-06 21:36:18 -0500

    Create initialPassword
    change auto-login

--- hosts/iNix/iNix.nix
+++ hosts/iNix/iNix.nix
@@ -24,7 +24,7 @@
       defaultSession = "qtile";
       autoLogin = {
         enable = true;
-        user = "wa";
+        user = "admin";
       };
     };
     openssh.settings = {
@@ -60,6 +60,7 @@
     };
     admin = {
       isNormalUser = true;
+      initialPassword = "changeme";
       extraGroups = [ "wheel" "networkmanager" "libvirtd" "kvm" ];
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ57DtlRJRHHceyg00N4PIswa4/sn/zA5nCInnX1Tka" ]; # mpNix public key
     };
--- hosts/uMix/uMix.nix
+++ hosts/uMix/uMix.nix
@@ -61,6 +61,7 @@
 
     admin = {
       isNormalUser = true;
+      initialPassword = "changeme";
       extraGroups = [ "wheel" "networkmanager" "libvirtd" "kvm"];
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ57DtlRJRHHceyg00N4PIswa4/sn/zA5nCInnX1Tka" ]; # mpNix public key
     };
