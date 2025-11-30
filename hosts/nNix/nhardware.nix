58000e01439c13d97190c312290ea91986aad37b
Curtis Abbott <inspiredplans@gmail.com>
2025-11-30 16:46:59 -0600

    remove raid1

--- hosts/nNix/nhardware.nix
+++ hosts/nNix/nhardware.nix
@@ -16,14 +16,14 @@
         "sd_mod"
       ];
       kernelModules = [ ];
-      };
-    swraid = {
-      enable = true;
-      mdadmConf = ''
-        MAILADDR root@localhost
-      '';
     };
-# # Set the resume device to the UUID of the swap partition
+#     swraid = {
+#       enable = true;
+#       mdadmConf = ''
+#         MAILADDR root@localhost
+#       '';
+#     };
+# # # Set the resume device to the UUID of the swap partition
 #     resumeDevice = lib.mkForce "/dev/disk/by-uuid/2d2042ab-b7f9-4289-9c73-8c03c366a708";
   # Set kernel parameters for hibernation
     kernelParams = [
