3db342e57059c8a7c5c74ae98494d19a248e1df6
Curtis Abbott <inspiredplans@gmail.com>
2025-11-30 20:31:01 -0600

    rearrange pre-swap

--- hosts/nNix/nhardware.nix
+++ hosts/nNix/nhardware.nix
@@ -18,14 +18,6 @@
       ];
       kernelModules = [ ];
     };
-    # swraid = {
-    #   enable = true;
-    #   mdadmConf = ''
-    #     MAILADDR root@localhost
-    #     ARRAY /dev/md127 level=raid1 num-devices=2 metadata=1.2 name=raid1 UUID=28328f99:0625df97:72836534:1327af59 devices=/dev/sda1,/dev/sdb1
-    #   '';
-    # };
-    # resumeDevice = lib.mkForce "/dev/disk/by-uuid/28328f99:0625df97:72836534:1327af59";
   # Set kernel parameters for hibernation
     kernelParams = [
       # "resume_offset=34816"
@@ -36,31 +28,15 @@
     ];
     extraModulePackages = [
     ];
-    # kernel.sysctl = { # Limit RAID resync speed so it doesn’t kill the system
-    #   "dev.raid.speed_limit_min" = 1000;
-    #   "dev.raid.speed_limit_max" = 100000;
-    # };
+    # resumeDevice = lib.mkForce "/dev/disk/by-uuid/28328f99:0625df97:72836534:1327af59";
   };
 
-# hardware.mdadm.arrays = {
-#     data = {
-#       level = 1;
-#       metadata = "1.2";
-#       devices = [
-#         "/dev/disk/by-id/ata-WDC_WD10JUCT-63CYNY0_WD-WXL1A56D5ND9-part1"
-#         "/dev/disk/by-id/ata-WDC_WD10JUCT-63CYNY0_WD-WX81EC59L6DM-part1"
-#       ];
-#       name = "data";
-#     };
-#   };
+  # swapDevices = [{
+  #   device = "/mnt/raid/swapfile";
+  #   size = 36 * 1024; # 36 GB in MB
+  # }];
 
   fileSystems = {
-  #   "/mnt/raid" = {
-  #     device = lib.mkForce "/dev/md127";
-  #     fsType = "ext4";
-  #     # options = [ "noatime" "rw" "uid=ca" "gid=users" "mode=0775" ];
-  #   };
-
     "/" = {
       device = lib.mkForce "/dev/disk/by-uuid/90f78c45-4232-49be-b19a-3b6960d4b88b";
       fsType = "ext4";
@@ -72,11 +48,6 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
   };
-
-  # swapDevices = [{
-  #   device = "/mnt/raid/swapfile";
-  #   size = 36 * 1024; # 36 GB in MB
-  # }];
 
   # Variables
   environment = {
